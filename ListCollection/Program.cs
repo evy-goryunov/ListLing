5b43486b64a39a57c81e462f57ffa7ff7a9a0d03
evy-goryunov <evy.goryunov@gmail.com>
2019-06-19 00:18:06 +0300

    2

--- ListCollection/Program.cs
+++ ListCollection/Program.cs
@@ -11,31 +11,25 @@ namespace ListCollection
 		static void Main(string[] args)
 		{
 
-			//List<int> list = new List<int>();
-			//Random rnd = new Random();
-			//int r = rnd.Next(1, 10);
+			List<int> list = new List<int>();
+			Random rnd = new Random();
+			int r = rnd.Next(1, 10);
 
+			Console.WriteLine("Заданный массив данных");
+			for (int i = 0; i < 10; i++)
+			{
+				list.Add(rnd.Next(1, 10));
+			}
+			foreach (var item in list)
+			{
+				Console.WriteLine(item);
+			}
+			Console.WriteLine("кол-во вхождений");
 
-			//for (int i = 0; i < 10; i++)
-			//{
-			//	list.Add(rnd.Next(1, 10));
-			//}
-			//foreach (var item in list)
-			//{
-			//	Console.WriteLine(item);
-			//}
-
-			//var num = from n in list where n > 5 select n;
-			//Console.WriteLine("Числа больше 5");
-			//foreach (var e in num)
-			//{
-			//	Console.WriteLine(e);
-			//}
-
-			NewWorker w1 = new NewWorker("Eugene", 31);
-			Console.WriteLine(w1.ToString());
-
-
+			foreach (int val in list.Distinct())
+			{
+				Console.WriteLine(val + " - " + list.Where(x => x == val).Count() + " шт");
+			}
 		}
 	}
 }
