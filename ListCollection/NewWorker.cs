fe8ad91e2faa88188a89fad8788de870b378dc45
evy-goryunov <evy.goryunov@gmail.com>
2019-06-18 21:43:22 +0300

    1

---
+++ ListCollection/NewWorker.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListCollection
+{
+		class NewWorker
+		{
+			public NewWorker(String Name, int age)
+			{
+				this.Name = Name;
+				this.age = age;
+			}
+
+			string Name { get; set; }
+			int age { get; set; }
+
+			public override string ToString()
+			{
+				return $"{this.Name,10}{this.age,2}";
+			}
+		}
+}
---
+++ ListCollection/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListCollection
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+
+			//List<int> list = new List<int>();
+			//Random rnd = new Random();
+			//int r = rnd.Next(1, 10);
+
+
+			//for (int i = 0; i < 10; i++)
+			//{
+			//	list.Add(rnd.Next(1, 10));
+			//}
+			//foreach (var item in list)
+			//{
+			//	Console.WriteLine(item);
+			//}
+
+			//var num = from n in list where n > 5 select n;
+			//Console.WriteLine("Числа больше 5");
+			//foreach (var e in num)
+			//{
+			//	Console.WriteLine(e);
+			//}
+
+			NewWorker w1 = new NewWorker("Eugene", 31);
+			Console.WriteLine(w1.ToString());
+
+
+		}
+	}
+}
